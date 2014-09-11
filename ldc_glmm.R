19e4e60559c7587c464c90c475d8b6995d56d2f8
Reid Miller <r24mille@uwaterloo.ca>
2014-09-11 13:50:56 -0400

    Demonstrate the best nested random effects to use, assuming no fixed effects (ie. start of Chapter 19).

--- ldc_glmm.R
+++ ldc_glmm.R
@@ -28,7 +28,45 @@ null_model <- lm(kwh ~ 1, readings)
 # Suppose that there are no fixed effects, so that all of the 
 # categorical variables are random effects. Then the fixed 
 # effect simply estimates the intercept (parameter 1):
+# fixed = kwh~1
 
+# The fixed effect is just the overall mean value of the response variable. The 
+# random effects show the indentities of the random variables and their relative 
+# locations in the hierarchy. The random effects are specified like:
+# random = ~ 1 | subject_reindexed
+
+# An important detail to notice is that the name of the response variable (kwh) 
+# is not repeated in the random-effects forumula: there is a blank space to the 
+# left of the tilde. In most mixed-effects models we assume that the random 
+# effects have a mean of zero and that we are interested in quantifying 
+# variation in the intercept (ie. parameter 1) caused by differences between 
+# the factor levels of the random effects.
+#
+# After the intercept comes the vertical bar | which is read as "given the 
+# following spatial arrangement of the random variables". In this case, there 
+# is ony random effect, subject_reindexed.
+#
+# TODO: random <- ~ 1 | subject_reindexed/daynum/hour
+# TODO: random <- ~ 1 | subject_reindexed/hourindex
+no_fixed_sub_only <- lme(fixed = kwh~1,
+                 random = ~ 1 | subject_reindexed)
+no_fixed_sub_nested_dayhour <- lme(fixed = kwh~1,
+                         random = ~ 1 | subject_reindexed/daynum/hour,
+                         control=lmeControl(opt="optim"))
+no_fixed_sub_hourindex <- lme(fixed = kwh~1,
+                         random = ~ 1 | subject_reindexed/hourindex,
+                         control=lmeControl(opt="optim"))
+no_fixed_sub_daynum <- lme(fixed = kwh~1,
+                               random = ~ 1 | subject_reindexed/daynum,
+                               control=lmeControl(opt="optim"))
+no_fixed_day_nested_sub <- lme(fixed = kwh~1,
+                           random = ~ 1 | daynum/subject_reindexed,
+                           control=lmeControl(opt="optim"))
+
+summary(no_fixed_sub_only)
+summary(no_fixed_sub_nested_dayhour)
+summary(no_fixed_sub_hourindex)
+summary(no_fixed_sub_daynum)
 
 # Linear Mixed-Effects Regression
 # Subject is a random effect because its factor levels have no meaning 
@@ -46,5 +84,4 @@ null_model <- lm(kwh ~ 1, readings)
 # an error.
 pseudorep_model <- lme(kwh~temperature*tou_period, 
                        random=~hourindex|subject_reindexed, 
-                       control=lmeControl(opt="optim")
-)
+                       control=lmeControl(opt="optim"))
