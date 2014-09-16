185d48efc647eac75317d45a57a315b3f7ba31d3
Reid Miller <r24mille@uwaterloo.ca>
2014-09-16 18:25:46 -0400

    Changed the aggregate GLMM to use a pre-computed .csv already generated by SQL. This allows for the use of all households in the aggregate group.

--- ldc_aggregate_glmm.R
+++ ldc_aggregate_glmm.R
@@ -7,23 +7,55 @@ library(stargazer) # LaTeX tables
 # Load SmartMeterReading data from CSV
 home <- setwd(Sys.getenv("HOME"))
 fpath <- file.path(home, 
-                   "../Dropbox/ISS4E/R/25_meterid_sample/", 
-                   "25_random_sample.csv")
-readings <- read.csv(fpath)
+                   "../Dropbox/ISS4E/R/", 
+                   "full_aggregate_readings.csv")
+readings.aggregate <- read.csv(fpath)
 
 # Re-orders TOU Period levels so that graphs are sorted accordingly
-readings$tou_period <- factor(readings$tou_period, 
+readings.aggregate$tou_period <- factor(readings.aggregate$tou_period, 
                               c("off_weekend", "off_morning", "mid_morning", 
                                 "on_peak", "mid_evening", "off_evening"))
 
-# Restructure meters into aggregate sample (ie. mean kWh for each hourindex).
-readings.aggregate <- ddply(readings, 
-                            ~hourindex, 
-                            summarize, 
-                            kwh = mean(kwh), 
-                            daynum = unique(daynum), 
-                            hour = unique(hour), 
-                            temperature = unique(temperature), 
-                            tou_period = unique(tou_period),
-                            billing_active = unique(billing_active), 
-                            agg_count = length(subject))
+##
+# A linear model will be build using lm(...) for each model variant so that 
+# we can find an R-squared value. Also, a general linear model 
+# will be fit through maximum likelihood estimation using glm(...). This 
+# provides additional metrics such as Akaike Information Criterion (AIC) to 
+# measure model quality.
+##
+
+# Temperature, TOU Period, and billing method modeled as fixed effects using 
+# using coefficients for main effects only.
+model.lm.fe.main.tmp_prd_bill <- lm(kwh ~ temperature + tou_period + billing_active, 
+                                    data = readings.aggregate)
+model.glm.fe.main.tmp_prd_bill <- glm(kwh ~ temperature + tou_period + billing_active, 
+                                      data = readings.aggregate)
+summary(model.lm.fe.main.tmp_prd_bill)
+summary(model.glm.fe.main.tmp_prd_bill)
+
+# Temperature, TOU Period, and billing method modeled as fixed effects using 
+# using coefficients of main effects and all interactions.
+model.lm.fe.inter.tmp_prd_bill <- lm(kwh ~ temperature * tou_period * billing_active, 
+                                         data = readings.aggregate)
+model.glm.fe.inter.tmp_prd_bill <- glm(kwh ~ temperature * tou_period * billing_active, 
+                                           data = readings.aggregate)
+summary(model.lm.fe.inter.tmp_prd_bill)
+summary(model.glm.fe.inter.tmp_prd_bill)
+
+# Temperature, hour of day, and billing method modeled as fixed effects using 
+# using coefficients for main effects only.
+model.lm.fe.main.tmp_hr_bill <- lm(kwh ~ temperature + hour + billing_active, 
+                                    data = readings.aggregate)
+model.glm.fe.main.tmp_hr_bill <- glm(kwh ~ temperature + hour + billing_active, 
+                                      data = readings.aggregate)
+summary(model.lm.fe.main.tmp_hr_bill)
+summary(model.glm.fe.main.tmp_hr_bill)
+
+# Temperature, hour of day, and billing method modeled as fixed effects using 
+# using coefficients of main effects and all interactions.
+model.lm.fe.inter.tmp_hr_bill <- lm(kwh ~ temperature * hour * billing_active, 
+                                     data = readings.aggregate)
+model.glm.fe.inter.tmp_hr_bill <- glm(kwh ~ temperature * hour * billing_active, 
+                                       data = readings.aggregate)
+summary(model.lm.fe.inter.tmp_hr_bill)
+summary(model.glm.fe.inter.tmp_hr_bill)
