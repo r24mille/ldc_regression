96ac56449cb6977b356de438d84f43b44b57b13e
Reid Miller <r24mille@uwaterloo.ca>
2014-09-18 17:00:49 -0400

    Now reads full_aggregate_readings.csv for dataframe and also added a visualization of the average kWh readings as a timeseries.

--- ldc_aggregate_count_visualization.R
+++ ldc_aggregate_count_visualization.R
@@ -1,16 +1,16 @@
-# Load count of SmartMeterReadings data from CSV
 home <- setwd(Sys.getenv("HOME"))
 fpath <- file.path(home, 
                    "../Dropbox/ISS4E/R/", 
-                   "observation_count_series.csv")
-readings.counts <- read.csv(fpath)
+                   "full_aggregate_readings.csv")
+readings.aggregate <- read.csv(fpath)
 
 # Convert the observation_timestamp column to Date
-readings.counts$observation_timestamp <- as.POSIXlt(readings.counts$observation_timestamp)
+readings.aggregate$timestamp_dst <- as.POSIXlt(readings.aggregate$timestamp_dst)
 
-# Make a simple plot of the data
-plot(readings.counts$observation_timestamp, 
-     readings.counts$number_observations,
+##
+# Make a simple plot of number of households involved in each hourly measurement
+plot(readings.aggregate$timestamp_dst, 
+     readings.aggregate$agg_count,
      main = "Count of Observations Each Hour", 
      ylab = paste("Number of Households with a Reading (max=", 
                   max(readings.counts$number_observations), 
@@ -23,6 +23,23 @@ plot(readings.counts$observation_timestamp,
      )
 tsrange <- as.POSIXct(round(range(readings.counts$observation_timestamp), 
                       "days"))
+axis.POSIXct(1, 
+             at = seq(tsrange[1], tsrange[2], by="month"), 
+             format="%m/%Y")
+
+##
+# Make a simple plot of the timeseries data
+plot(readings.aggregate$timestamp_dst, 
+     readings.aggregate$kwh,
+     main = "Timeseries of Aggregate Average Reading", 
+     ylab = paste("Average Readings (kWh)"),
+     xlab = "Date (hourly)", 
+     type = "l", # Line
+     col = rgb(0, 0, 0, 1, maxColorValue=1), 
+     xaxt = "n" # x-axis type "n" turns of ticks
+)
+tsrange <- as.POSIXct(round(range(readings.counts$observation_timestamp), 
+                            "days"))
 axis.POSIXct(1, 
              at = seq(tsrange[1], tsrange[2], by="month"), 
              format="%m/%Y")
