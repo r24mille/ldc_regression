803ce1529c5cef5f59e6033a75f903a64b6fa734
Reid Miller <r24mille@uwaterloo.ca>
2014-10-02 21:35:24 -0400

    Commenting my TODOs with notes so that I can make sound decisions about various model choices.

--- ldc_aggregate_glmm.R
+++ ldc_aggregate_glmm.R
@@ -57,7 +57,21 @@ readings.aggregate$price <- factor(readings.aggregate$price,
 
 ##
 # Weight terms based on the number of readings involved in the aggregate average
+#
 # TODO(r24mille): Resid. Dev is lower with weights. Justify this choice.
+#                   * Weights really have more to do with dispersion and non-
+#                     homogeneity of variance. 
+#                   * From ?glm it states, "Non-NULL weights can be used to
+#                     indicate that different observations have different
+#                     dispersions (with the values in weights being inversely
+#                     proportional to the dispersions); or equivalently, when the
+#                     elements of weights are positive integers w_i, that each
+#                     response y_i is the mean of w_i unit-weight observations. For
+#                     a binomial GLM prior weights are used to give the number of
+#                     trials when the response is the proportion of successes: they
+#                     would rarely be used for a Poisson GLM."
+#                   * The 'number of trials' doesn't quite fit with agg_count the 
+#                     way it is written below.
 maxagg <- max(readings.aggregate$agg_count)
 wghts <- readings.aggregate$agg_count/maxagg
 
@@ -91,8 +105,11 @@ readings.aggregate$cdh <- ifelse(readings.aggregate$temperature > cdhbreak,
 # or should previous hours be nested under the current hour?
 #
 # TODO(r24mille): Resid. Dev is lower with link="log". Justify this choice.
+#                   * This is the same as lm(log(kwh) ~ .) but allows for 
+#                     easier interpretation of the expected value since it does 
+#                     not need to be back transformed
 # TODO(r24mille): This whole section could be two nicely built functions
-nlags <- 2
+nlags <- 12
 
 # 1. Summed CDH lags, TOU as periods
 cdhlagsum.touperiods.maxglm.pwr <- matrix(nrow = (nlags + 1),
