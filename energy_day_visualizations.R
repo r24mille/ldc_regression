3e4d30668219610492b55d9816139a5f9c59162f
Reid Miller <r24mille@uwaterloo.ca>
2014-10-20 12:12:40 -0400

    Added weekend plot to Energy Day visualization and am commenting out confidence intervals for now.

--- energy_day_visualizations.R
+++ energy_day_visualizations.R
@@ -26,7 +26,7 @@ plot(x = readings.aggregate.2012$temperature,
 # Working GLM for results
 df.trimmed$hour <- readings.aggregate$hour
 working.glm <- glm(data = df.trimmed, 
-                   formula = "kwh ~ billing_active + month + hrstr + weekend + price + lag0 + lag1 + lag2 + lag3 + lag4 + lag5 + billing_active:month + billing_active:weekend + billing_active:price + billing_active:lag0 + billing_active:lag1 + billing_active:lag2 + billing_active:lag3 + billing_active:lag4 + billing_active:lag5 + month:weekend + month:price + month:lag0 + month:lag1 + month:lag5 + hrstr:lag0 + hrstr:lag5 + weekend:lag0 + weekend:lag1 + weekend:lag2 + weekend:lag3 + weekend:lag4 + weekend:lag5 + price:lag0 + price:lag1 + price:lag5 + lag0:lag1 + lag0:lag2 + lag0:lag3 + lag0:lag4 + lag0:lag5 + lag1:lag2 + lag1:lag3 + lag1:lag4 + lag1:lag5 + lag2:lag3 + lag2:lag4 + lag2:lag5 + lag3:lag4 + lag3:lag5 + lag4:lag5", 
+                   formula = "kwh ~ billing_active + month + hrstr + weekend + price + lag0 + lag1 + lag2 + lag3 + lag4 + lag5 + lag6 + lag7 + lag8 + billing_active:month + billing_active:weekend + billing_active:lag7 + billing_active:lag8 + month:hrstr + month:weekend + month:price + month:lag0 + month:lag2 + month:lag5 + month:lag8 + hrstr:weekend + hrstr:lag0 + hrstr:lag1 + hrstr:lag2 + hrstr:lag3 + hrstr:lag4 + hrstr:lag5 + hrstr:lag6 + hrstr:lag7 + hrstr:lag8 + weekend:lag2 + price:lag1 + price:lag8 + lag0:lag3 + lag0:lag6 + lag1:lag2 + lag1:lag4 + lag2:lag3 + lag2:lag5 + lag3:lag4 + lag3:lag5 + lag3:lag6 + lag3:lag7 + lag4:lag6 + lag4:lag7 + lag5:lag7", 
                    family = Gamma(link = "log"))
 df.trimmed.2012 <- subset(df.trimmed, billing_active == "Yes")
 df.trimmed.2012.whatif <- df.trimmed.2012
@@ -67,6 +67,11 @@ df.trimmed.2012.weekday.avg <- ddply(df.trimmed.2012.weekday,
                                      summarize,
                                      kwh = mean(kwh),
                                      hour = unique(hour))
+df.trimmed.2012.weekend.avg <- ddply(df.trimmed.2012.weekend,
+                                     ~hrstr,
+                                     summarize,
+                                     kwh = mean(kwh),
+                                     hour = unique(hour))
 df.trimmed.2012.weekday.tou <- ddply(df.trimmed.2012.weekday,
                                      ~price,
                                      summarize,
@@ -90,6 +95,13 @@ df.trimmed.2012.whatif.weekday.avg <- ddply(df.trimmed.2012.whatif.weekday,
                                      kwh = mean(kwh),
                                      conf.upper = mean(conf.upper),
                                      hour = unique(hour))
+df.trimmed.2012.whatif.weekend.avg <- ddply(df.trimmed.2012.whatif.weekend,
+                                            ~hrstr,
+                                            summarize,
+                                            conf.lower = mean(conf.lower),
+                                            kwh = mean(kwh),
+                                            conf.upper = mean(conf.upper),
+                                            hour = unique(hour))
 df.trimmed.2012.whatif.weekday.tou <- ddply(df.trimmed.2012.whatif.weekday,
                                      ~price,
                                      summarize,
@@ -124,8 +136,7 @@ overall_avg_pcnt <- 100 - 100 * (mean(df.trimmed.2012.whatif$kwh) / mean(df.trim
 overall_upp_pcnt <- 100 - 100 * (mean(df.trimmed.2012.whatif$conf.upper) / mean(df.trimmed.2012$kwh))
 
 # Plot the average summer weekday
-yrng <- c((min(df.trimmed.2012.whatif.weekday.avg$conf.lower) - 0.25), 
-          (max(df.trimmed.2012.whatif.weekday.avg$conf.upper) + 0.25))
+yrng <- c(0.5, 2.2)
 obscol <- "black"
 predcol <- "blue"
 confcol <- "grey"
@@ -140,22 +151,22 @@ plot(x = df.trimmed.2012.whatif.weekday.avg$hour,
      lwd = 2, 
      lty = 2, 
      col = predcol)
-)
+
 axis(side = 1, # x-axis
      at = c(0:23), 
      labels = c(0, "", 2, "", 4, "", 6, "", 8, "", 10, "", 12, "", 14, "", 16, "", 18, "", 20, "", 22, ""))
-lines(x = df.trimmed.2012.whatif.weekday.avg$hour,
-      y = df.trimmed.2012.whatif.weekday.avg$conf.lower,
-      type = "l",
-      lwd = 2, 
-      lty = 3, # dashed
-      col = confcol)
-lines(x = df.trimmed.2012.whatif.weekday.avg$hour,
-      y = df.trimmed.2012.whatif.weekday.avg$conf.upper,
-      type = "l",
-      lwd = 2, 
-      lty = 3, # dashed
-      col = confcol)
+# lines(x = df.trimmed.2012.whatif.weekday.avg$hour,
+#       y = df.trimmed.2012.whatif.weekday.avg$conf.lower,
+#       type = "l",
+#       lwd = 2, 
+#       lty = 3, # dashed
+#       col = confcol)
+# lines(x = df.trimmed.2012.whatif.weekday.avg$hour,
+#       y = df.trimmed.2012.whatif.weekday.avg$conf.upper,
+#       type = "l",
+#       lwd = 2, 
+#       lty = 3, # dashed
+#       col = confcol)
 lines(x = df.trimmed.2012.weekday.avg$hour, 
       y = df.trimmed.2012.weekday.avg$kwh,
       type = "l", 
@@ -166,5 +177,51 @@ legend("topleft",
        lty = c(1, 2, 3),
        lwd = c(1, 2, 2), # line width
        legend = c("Observed Demand with TOU Billing",
-                  "Predicted Demand without TOU Billing", 
-                  "95% Confidence Interval"))
+                  "Predicted Demand without TOU Billing"))#, 
+                  #"95% Confidence Interval"))
+
+
+# Plot the average summer weekend
+yrng <- c(0.5, 2.2)
+obscol <- "black"
+predcol <- "blue"
+confcol <- "grey"
+plot(x = df.trimmed.2012.whatif.weekend.avg$hour, 
+     y = df.trimmed.2012.whatif.weekend.avg$kwh,
+     ylim = yrng,
+     ylab = "Average Household Demand (kWh)",
+     xaxt = "n", 
+     xlab = "Hour of Day",
+     main = "Average Household Electricity Demand during a Weekend, Summer 2012",
+     type = "l",
+     lwd = 2, 
+     lty = 2, 
+     col = predcol)
+
+axis(side = 1, # x-axis
+     at = c(0:23), 
+     labels = c(0, "", 2, "", 4, "", 6, "", 8, "", 10, "", 12, "", 14, "", 16, "", 18, "", 20, "", 22, ""))
+# lines(x = df.trimmed.2012.whatif.weekend.avg$hour,
+#       y = df.trimmed.2012.whatif.weekend.avg$conf.lower,
+#       type = "l",
+#       lwd = 2, 
+#       lty = 3, # dashed
+#       col = confcol)
+# lines(x = df.trimmed.2012.whatif.weekend.avg$hour,
+#       y = df.trimmed.2012.whatif.weekend.avg$conf.upper,
+#       type = "l",
+#       lwd = 2, 
+#       lty = 3, # dashed
+#       col = confcol)
+lines(x = df.trimmed.2012.weekend.avg$hour, 
+      y = df.trimmed.2012.weekend.avg$kwh,
+      type = "l", 
+      lwd = 1, 
+      col = obscol)
+legend("topleft", 
+       col = c(obscol, predcol, confcol), 
+       lty = c(1, 2, 3),
+       lwd = c(1, 2, 2), # line width
+       legend = c("Observed Demand with TOU Billing",
+                  "Predicted Demand without TOU Billing"))#, 
+                  #"95% Confidence Interval"))
