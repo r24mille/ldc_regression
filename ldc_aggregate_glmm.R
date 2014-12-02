1d1dd376d1b5beadf690518dada6e30610d1dc6d
Reid Miller <r24mille@uwaterloo.ca>
2014-12-02 17:19:26 -0500

    Created several plots to visualize the quality of model fit by hierarchical group-LASSO.

--- ldc_aggregate_glmm.R
+++ ldc_aggregate_glmm.R
@@ -1,4 +1,5 @@
 library(segmented) # Find linear regression breakpoint
+library(ggplot) # Better plots!
 library(glinternet) # Hierarchical group-LASSO variable selection
 
 # Source functions in other files
@@ -63,3 +64,51 @@ cv.hglasso <- glinternet.cv(X = X.mat,
                             Y = Y.mat, 
                             numLevels = numlvl.vec, 
                             family = "gaussian")
+
+# Backtransform optimal, fitted values and compute more meaningful RMSE and MAPE
+Y.bktr <- exp(Y.mat)
+hglasso.fitted.bktr <- exp(cv.hglasso$fitted)
+hglasso.rmse.bktr <- sqrt(mean((hglasso.fitted.bktr - Y.bktr)^2, 
+                                     na.rm = TRUE))
+hglasso.mape.bktr <- (1/nrow(Y.bktr)) * sum(abs((Y.bktr - hglasso.fitted.bktr)/Y.bktr))
+ghlasso.residuals.bktr <- Y.bktr - hglasso.fitted.bktr
+
+# QQ Plot of fitted vs. observed values
+library(car) # Companion to Applied Regression (better qqPlot)
+qqPlot(x = ghlasso.residuals.bktr,
+       distribution = "norm",
+       envelope = .95,
+       mean = 0,
+       main = "Hierarchical Group-LASSO Residuals vs. Normal Distribution",
+       ylab = "Backtransformed Residuals (kWh)",
+       xlab = "Norm Quantiles")
+
+# Plot the histogram of residuals
+plot(density(ghlasso.residuals.bktr),
+     main = "Histogram of Residuals",
+     xlab = "Backtransformed Residuals (kWh)")
+abline(v = mean(ghlasso.residuals.bktr),
+       col = "darkgray",
+       lty = 2)
+axis(3, # top label
+     padj = 1, # align to bottom of label bounding box
+     at = round(mean(ghlasso.residuals.bktr), 3))
+
+# Plot residuals to look for patterns
+ghlasso.df.resids.bktr <- data.frame(ghlasso.residuals.bktr, 
+                                     readings.aggregate$timestamp_dst)
+names(ghlasso.df.resids.bktr) <- c("res", "dst")
+ggresiduals <- (ggplot(ghlasso.df.resids.bktr, aes(y = res, x = dst))
+                + geom_point(alpha = 1/3)
+                + labs(x = "Date (hourly)", 
+                       y = "Backtransformed Residuals (kWh)",
+                       title= paste("Residuals of Optimal Hierarchical Group-LASSO Model",
+                                    "\n(ie. Observed - Fitted)"))
+                + scale_x_datetime(labels = date_format("%b %Y"), 
+                                   breaks = "1 month")
+                + theme(axis.text.x = element_text(angle=90, 
+                                                   vjust=0.5, 
+                                                   size=12),
+                        axis.text.y = element_text(size=12))
+)
+ggresiduals
